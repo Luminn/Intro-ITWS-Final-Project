cd1a4abbeeb419bcbe2d68c3988b75b119d7ea6c
GeordieTate <tateg@rpi.edu>
2017-12-04 09:44:56 -0500

    updated and improved JS for slideshow as well as CSS

--- resources/slideshow.js
+++ resources/slideshow.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-
+   showSlides(1)
+})
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,4 +27,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
-})
